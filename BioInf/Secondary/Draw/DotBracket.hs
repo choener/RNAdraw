854742c870f4301cdefa3b0ee1f3cad919554c08
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2011-10-17 16:59:40 +0200

    extended the drawing system by allowing to return "parts"

--- BioInf/Secondary/Draw/DotBracket.hs
+++ BioInf/Secondary/Draw/DotBracket.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TupleSections #-}
 
@@ -11,6 +12,7 @@ import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Vector.Unboxed as V
 import Text.Printf
+import Prelude hiding (sequence)
 
 import Biobase.Secondary
 
@@ -22,24 +24,37 @@ class DotBracketDraw a where
   -- | Draw a secondary structure. It is assumed that the structure is
   -- pseudoknot-free.
   draw :: PairAnno -> SequenceNumbering -> a -> String
-
+  -- | As 'draw' but returns the different strings as parts. These can then be
+  -- further altered by the receiving end.
+  drawParts :: PairAnno -> SequenceNumbering -> a -> Parts
   -- | Draw a pseudoknotted secondary structures.
   drawPK :: PairAnno -> SequenceNumbering -> a -> String
+
+data Parts = Parts
+  { numbers :: Maybe String
+  , sequence :: String
+  , structure :: String
+  , extended :: [String]
+  }
 
 -- | 
 
 instance DotBracketDraw (String,[ExtPairIdx]) where
   draw pa sn (seq,xs) = unlines $
-    [ take l . concat $ repeat ['0'..'9']
-    | sn == Numbered
-    ] ++
-    [ seq
-    , V.toList $ V.accum updateStructure (V.fromList $ replicate l '.') (map ((,'(').baseL) xs ++ map ((,')').baseR) xs)
-    ] ++
-    [ printf "  %4d %4d   %c-%c   %s" (baseL x) (baseR x) (seq!!baseL x) (seq!!baseR x) (show $ baseT x)
-    | x <- xs
-    , pa == Always || pa == Required && baseT x /= cWW
-    ] where
+    maybe [] (:[]) numbers ++
+    [ sequence
+    , structure
+    ] ++ extended
+    where
+      Parts{..} = drawParts pa sn (seq,xs)
+  drawParts pa sn (seq,xs) = Parts
+    { numbers = if sn==Numbered then Just . take l . concat $ repeat ['0'..'9'] else Nothing
+    , sequence = seq
+    , structure = V.toList $ V.accum updateStructure (V.fromList $ replicate l '.') (map ((,'(').baseL) xs ++ map ((,')').baseR) xs)
+    , extended = [ printf "  %4d %4d   %c-%c   %s" (baseL x) (baseR x) (seq!!baseL x) (seq!!baseR x) (show $ baseT x)
+                 | x <- xs, pa == Always || pa == Required && baseT x /= cWW
+                 ]
+    } where
       l = length seq
       updateStructure cur new
         | cur == '.' = new
@@ -50,12 +65,15 @@ instance DotBracketDraw (String,[ExtPairIdx]) where
 
 instance DotBracketDraw (String,[PairIdx]) where
   draw pa sn (seq,xs) = draw pa sn (seq,map (\x -> ((baseL x, baseR x), baseT x)) xs)
+  drawParts pa sn (seq,xs) = drawParts pa sn (seq, map (\x -> ((baseL x, baseR x), baseT x)) xs)
 
 instance DotBracketDraw (ByteString,[PairIdx]) where
   draw pa sn (seq,xs) = draw pa sn (BS.unpack seq,xs)
+  drawParts pa sn (seq,xs) = drawParts pa sn (BS.unpack seq, xs)
 
 instance DotBracketDraw (ByteString,[ExtPairIdx]) where
   draw pa sn (seq,xs) = draw pa sn (BS.unpack seq,xs)
+  drawParts pa sn (seq,xs) = drawParts pa sn (BS.unpack seq, xs)
 
 -- | How to handle the list of annotated pairs.
 
